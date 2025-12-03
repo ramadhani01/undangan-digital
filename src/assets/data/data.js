bd255a36623f15f593de9bffdbc3e14290c13ee0
ramadhani01 <45001162+ramadhani01@users.noreply.github.com>
2025-12-03 15:56:30 +0700

    data

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -2,18 +2,18 @@ export const data = {
     bride: {
         L: {
             id: 1,
-            name: 'Lorem Ipsum',
-            child: 'Putra ke lorem',
-            father: 'Lorem',
-            mother: 'Ipsum',
+            name: 'Ary Amanza',
+            child: 'Putra ke satu',
+            father: 'Rahman Adji Amanza',
+            mother: 'Sri Puspita Sari Amanza',
             image: './src/assets/images/cowo.png'
         },
         P: {
             id: 2,
-            name: 'Ipsum Lorem',
-            child: 'Putri ke lorem',
-            father: 'Lorem',
-            mother: 'Ipsum',
+            name: 'Liana Chilza Dewi',
+            child: 'Putri ke dua',
+            father: 'Adji Guntur Purnomo',
+            mother: 'Ayudia Lasmi Dewi',
             image: './src/assets/images/cewe.png'
         },
 
@@ -22,8 +22,8 @@ export const data = {
 
     time: {
         marriage: {
-            year: '2030',
-            month: 'November',
+            year: '2026',
+            month: 'Februari',
             date: '14',
             day: 'Kamis',
             hours: {
@@ -32,16 +32,16 @@ export const data = {
             }
         },
         reception: {
-            year: '2024',
-            month: 'November',
-            date: '14',
-            day: 'Kamis',
+            year: '2026',
+            month: 'Januari',
+            date: '3',
+            day: 'rabu',
             hours: {
                 start: '11.00',
                 finish: 'Selesai'
             }
         },
-        address: 'Kp. Lorem, RT 000/ RW 000, Desa.Lorem, Kec.Ipsum, Kab.Lorem, Lorem (1234)'
+        address: 'Shadiqsharia, Jl. Kolonel Masturi No.33, RW.03, Cimahi, Kec. Cimahi Tengah, Kota Cimahi, Jawa Barat 40525'
     },
 
     link: {
@@ -70,21 +70,6 @@ export const data = {
             id: 5,
             image: './src/assets/images/5.png'
         }
-    ],
-
-    bank: [
-        {
-            id: 1,
-            name: 'Lorem Ipsum',
-            icon: './src/assets/images/bca.png',
-            rekening: '12345678'
-        },
-        {
-            id: 2,
-            name: 'Ipsum Lorem',
-            icon: './src/assets/images/bri.png',
-            rekening: '12345678'
-        },
     ],
 
     audio: './src/assets/audio/wedding.mp3',
